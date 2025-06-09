075a21b48e30dca576d1d9edab794b81cad8ed15
PabloMakesStuff <vasicpavle47@gmail.com>
2025-06-09 21:37:01 +0200

    Dodat grid based movement

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,25 +1,27 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var CurrPos = Vector2()
+var move_cooldown = 0.15
+var time_since_move = 0.0
 
+func _physics_process(delta):
+	time_since_move += delta
+	if time_since_move >= move_cooldown:
+		if Input.is_action_pressed("ui_right"):
+			CurrPos.x += 32
+			time_since_move = 0
+			
+		elif Input.is_action_pressed("ui_left"):
+			CurrPos.x -= 32
+			time_since_move = 0
+			
+		elif Input.is_action_pressed("ui_down"):
+			CurrPos.y += 32
+			time_since_move = 0
+			
+		elif Input.is_action_pressed("ui_up"):
+			CurrPos.y -= 32
+			time_since_move = 0
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	self.position = CurrPos
